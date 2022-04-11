24dd5fb53103550be0cd0d23f244f1f16fd5862c
harrymuluk <hareshmuluk05@gmail.com>
2022-04-11 11:58:03 +0530

    first commit

---
+++ Day2_intro_to_java/src/com/cg/git/Intro_example1.java
@@ -0,0 +1,20 @@
+package com.cg.git;
+
+public class Intro_example1 {
+
+	public static void main(String[] args) {
+		int a=15;//whole
+		float b=10.5f;//decimal point
+		double c=14.7;//decimal point
+		char d='x';
+		String str="Haresh Muluk";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+		
+
+	}
+
+}
