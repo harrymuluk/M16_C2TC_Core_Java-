5cd416b0f60b498ad7282223471779b0d922a44b
harrymuluk <hareshmuluk05@gmail.com>
2022-04-11 12:00:24 +0530

    first commit

---
+++ Assignment_1/src/com/haresh/operator/Pattern_for_loop.java
@@ -0,0 +1,18 @@
+package com.haresh.operator;
+
+public class Pattern_for_loop {
+
+	public static void main(String[] args) {
+		int i,j,n=5;
+		for(i=1;i<=n;i++)
+		{
+		for(j=1;j<=i;j++)
+		System.out.print("#");
+		System.out.println("");
+
+		}
+
+
+	}
+
+}
---
+++ Assignment_1/src/com/haresh/operator/do_while_example.java
@@ -0,0 +1,15 @@
+package com.haresh.operator;
+
+public class do_while_example {
+
+	public static void main(String[] args) {
+	    int i=1;    
+	    do{    
+	        System.out.println("Haresh");    
+	    i++;    
+	    }while(i<=5);    
+
+
+	}
+
+}
